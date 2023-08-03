7564bf55198e6deebf50105b26efb0efebd36f40
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-03 10:32:51 +0900

    initial commiut

---
+++ src/test/java/com/be05/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be05.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
