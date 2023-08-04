0230bb84a77b0569046f8dddfe7b45c76a458a62
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-04 13:14:05 +0900

    feat: security 설정

---
+++ src/main/java/com/be05/sns/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.be05.sns.config;
+
+import com.be05.sns.token.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // CSRF 보안 비활성화
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authHttp -> authHttp
+                        .requestMatchers("/")
+                        // 모든 접근 허용(나머지 인증 후 허용)
+                        .anonymous().anyRequest().authenticated()
+                ).sessionManagement(sessionManagement -> sessionManagement
+                        // 세션 (생성 X, STATELESS), JWT 토큰 기반 인증을 사용하기 위한 설정
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                // AuthorizationFilter 전 jwtTokenFilter
+                ).addFilterBefore(jwtFilter, AuthorizationFilter.class);
+
+        return http.build();
+    }
+}
