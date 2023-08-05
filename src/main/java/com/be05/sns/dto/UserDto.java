39202c198d113f2cb8a8ad1298a08542f14b8b2c
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-05 23:49:53 +0900

    modified: UserDto 변경 - 변수 이름 설정 - 주석 및 반환 값 변경 - fromEntity 메서드 추가(entity -> dto) - newUser 메서드 추가(encoded Password 받아옴)

--- src/main/java/com/be05/sns/dto/UserDto.java
+++ src/main/java/com/be05/sns/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.be05.sns.dto;
 
+import com.be05.sns.entity.Users;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,12 +9,15 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
-@Data @Builder @AllArgsConstructor
+@Data
+@Builder
+@AllArgsConstructor
 public class UserDto implements UserDetails {
+
     private Long id;
-    private String userName;
+    private String username;
     private String password;
-    private String profile_img;
+    private String profileImg;
     private String email;
     private String phone;
     private String token;
@@ -24,30 +28,54 @@ public class UserDto implements UserDetails {
     }
 
     public String getUsername() {
-        return this.userName;
+        return this.username;
     }
 
     public String getPassword() {
         return this.password;
     }
 
-    @Override
+    @Override // 계정 만료
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
-    @Override
+    @Override // 계정 락
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
-    @Override
+    @Override // 자격 증명 만료
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
-    @Override
+    @Override // 계정 활성화
     public boolean isEnabled() {
-        return false;
+        return true;
+    }
+
+    public static UserDto fromEntity(Users user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .profileImg(user.getProfileImg())
+                .email(user.getEmail())
+                .phone(user.getPhone())
+                .token(user.getToken())
+                .build();
+    }
+
+    // 새로운 User 생성
+    public Users newUser(String encodedPassword) {
+        Users user = new Users();
+        user.setUsername(username);
+        user.setPassword(encodedPassword);
+        user.setProfileImg(profileImg);
+        user.setEmail(email);
+        user.setPhone(phone);
+        user.setToken(token);
+        return user;
     }
 }
