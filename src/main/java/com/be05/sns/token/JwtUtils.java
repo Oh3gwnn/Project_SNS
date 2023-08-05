adf3db16bedc52c30526b163d1515c23c2f923b9
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-05 23:46:43 +0900

    modified: UserDto -> UserDetails

--- src/main/java/com/be05/sns/token/JwtUtils.java
+++ src/main/java/com/be05/sns/token/JwtUtils.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -27,9 +28,9 @@ public class JwtUtils {
     }
 
     // JWT 생성
-    public String generateToken(UserDto userDto) {
+    public String generateToken(UserDetails userDetails) {
         Claims jwtClaims = Jwts.claims()
-                .setSubject(userDto.getUsername())
+                .setSubject(userDetails.getUsername())
                 .setIssuedAt(Date.from(Instant.now()))
                 .setExpiration(Date.from(Instant.now().plusSeconds(3600)));
 
