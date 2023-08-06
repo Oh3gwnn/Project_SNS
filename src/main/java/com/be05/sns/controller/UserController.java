65ce5928473235e7c1a8f8a949c7da5905275f19
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-07 06:51:09 +0900

    modified: uploadProfile 추가  - POST Mapping  - MediaType.MULTIPART_FORM_DATA_VALUE

--- src/main/java/com/be05/sns/controller/UserController.java
+++ src/main/java/com/be05/sns/controller/UserController.java
@@ -4,9 +4,10 @@ import com.be05.sns.dto.ResponseDto;
 import com.be05.sns.dto.UserDto;
 import com.be05.sns.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,5 +27,14 @@ public class UserController {
     public ResponseDto signUp(@RequestBody UserDto dto) {
         userService.createUser(dto);
         return response.toMessage("회원가입 성공");
+    }
+
+    // 3. 프로필 업로드
+    @PostMapping(value = "/upload/profile",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseDto uploadProfile(@RequestParam("profile") MultipartFile profileFile,
+                                     Authentication authentication) {
+        userService.uploadImage(profileFile, authentication);
+        return response.toMessage("프로필 업로드 성공");
     }
 }
