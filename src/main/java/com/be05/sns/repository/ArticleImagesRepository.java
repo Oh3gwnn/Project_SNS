adef611ba68e77096ec2609571b79129e7deca69
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-08 07:42:01 +0900

    modified: (첫 번째 이미지 + 원하는 피드 아이디) 해당항목 찾기

--- src/main/java/com/be05/sns/repository/ArticleImagesRepository.java
+++ src/main/java/com/be05/sns/repository/ArticleImagesRepository.java
@@ -2,6 +2,9 @@ package com.be05.sns.repository;
 
 import com.be05.sns.entity.ArticleImages;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleImagesRepository extends JpaRepository<ArticleImages, Long> {
+    ArticleImages findByIdAndArticleId_Id(Long id, Long articleId);
 }
