577f502f4947f5c80e6b9997caf4fc94cde1f3fc
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-07 17:56:47 +0900

    modified: Authentication 제거 + URL 간소화

--- src/main/java/com/be05/sns/repository/ArticleRepository.java
+++ src/main/java/com/be05/sns/repository/ArticleRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     Page<Article> findAllByUserId_Username(String username, Pageable pageable);
-    Optional<Article> findByIdAndUserId_Id(Long userid, Long articleId);
 }
