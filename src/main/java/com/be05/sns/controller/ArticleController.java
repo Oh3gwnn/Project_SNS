e14db526d70e580cc278f05a1708d106dc3563e7
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-07 17:30:58 +0900

    modified: create -> Json에서 form-data 형식으로 변경, 여러 이미지 업로드 추가

--- src/main/java/com/be05/sns/controller/ArticleController.java
+++ src/main/java/com/be05/sns/controller/ArticleController.java
@@ -2,13 +2,15 @@ package com.be05.sns.controller;
 
 import com.be05.sns.dto.Article.AUserFeedDto;
 import com.be05.sns.dto.Article.UserFeedsDto;
-import com.be05.sns.dto.ArticleDto;
 import com.be05.sns.dto.ResponseDto;
 import com.be05.sns.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,10 +19,12 @@ public class ArticleController {
     private final ResponseDto response;
     private final ArticleService articleService;
 
-    @PostMapping("/create")
-    public ResponseDto createdFeed(@RequestBody ArticleDto dto,
-                                   Authentication authentication) {
-        articleService.createFeed(dto, authentication);
+    @PostMapping(value = "/create")
+    public ResponseDto createdFeed(
+            @RequestParam String title, @RequestParam String content,
+            @RequestPart(value = "image", required = false) List<MultipartFile> imageFiles,
+            Authentication authentication) {
+        articleService.createFeed(title, content, imageFiles, authentication);
         return response.toMessage("피드를 생성했습니다.");
     }
 
