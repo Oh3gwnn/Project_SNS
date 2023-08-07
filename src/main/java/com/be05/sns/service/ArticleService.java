221e8fad1d26f007408122a680677cd9676e852a
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-07 17:34:22 +0900

    modified: createFeed 수정 - json -> form-data 변경으로 인한 newArticle 수정 - 이미지 파일 리스트를 가져와서 isempty 확인 후 uploadImages 메서드로 - uploadImages -> for each로 한 게시물(articleId)에 대한 이미지들 저장

--- src/main/java/com/be05/sns/service/ArticleService.java
+++ src/main/java/com/be05/sns/service/ArticleService.java
@@ -4,10 +4,13 @@ import com.be05.sns.dto.Article.AUserFeedDto;
 import com.be05.sns.dto.Article.UserFeedsDto;
 import com.be05.sns.dto.ArticleDto;
 import com.be05.sns.entity.Article;
+import com.be05.sns.entity.ArticleImages;
 import com.be05.sns.entity.Users;
+import com.be05.sns.repository.ArticleImagesRepository;
 import com.be05.sns.repository.ArticleRepository;
 import com.be05.sns.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,23 +18,57 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleRepository articleRepository;
+    private final ArticleImagesRepository imagesRepository;
     private final UserRepository userRepository;
+    private final ImageFormattingService imageFormatting;
+    private final ModelMapper modelMapper;
 
     // 1. create feed
-    public void createFeed(ArticleDto dto, Authentication authentication) {
+    public void createFeed(String title, String content,
+                           List<MultipartFile> imageFiles,
+                           Authentication authentication) {
         Users user = userRepository.findByUsername(authentication.getName())
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
-        articleRepository.save(dto.newArticle(user));
+        ArticleDto articleDto = new ArticleDto().newArticle(user, title, content);
+        Article article = modelMapper.map(articleDto, Article.class); // ModelConfig 참고
+        articleRepository.save(article);
+
+        if (imageFiles != null && !imageFiles.isEmpty()) {
+            uploadImages(authentication, article, imageFiles);
+            articleRepository.save(article);
+        }
     }
+
+    // 1-1. 여러 이미지 업로드
+    private void uploadImages(Authentication authentication,
+                              Article article,
+                              List<MultipartFile> imageFiles) {
+        String userName = authentication.getName();
+        String dirPath = String.format("images/article/%d/", article.getId());
+
+        int number = 0;
+        for (MultipartFile imageFile : imageFiles) {
+            ArticleImages articleImages = new ArticleImages();
+            String fileName = imageFormatting
+                    .uploadImage(imageFile, String.valueOf(++number), dirPath);
+
+            articleImages.setArticleId(article);
+            articleImages.setImageUrl(String.format("/static/%s", fileName));
+            imagesRepository.save(articleImages);
+        }
+    }
+
     // 2. writer all feed
     public Page<UserFeedsDto> readAllFeed(Long userId, Authentication authentication) {
         Users writer = userRepository.findById(userId)
