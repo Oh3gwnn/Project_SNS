95a99fde5b4bb8b8b9d10f92bbccead77b7a540e
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-08 08:23:08 +0900

    modified: List<String> imageUrls 추가 - imageUrl을 List로 가져옴

--- src/main/java/com/be05/sns/dto/Article/AUserFeedDto.java
+++ src/main/java/com/be05/sns/dto/Article/AUserFeedDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Data
 @Builder
@@ -13,14 +14,16 @@ public class AUserFeedDto {
     private String title;
     private String content;
     private String createdAt;
+    private List<String> imageUrls;
 
-    public static AUserFeedDto fromFeedInfo(Article article) {
+    public static AUserFeedDto fromFeedInfo(Article article, List<String> imageUrls) {
         return AUserFeedDto.builder()
                 .userName(article.getUserId().getUsername())
                 .title(article.getTitle())
                 .content(article.getContent())
                 .createdAt(article.getCreatedAt()
                         .format(DateTimeFormatter.ofPattern("MM월 dd일(E) a HH시 mm분")))
+                .imageUrls(imageUrls)
                 .build();
     }
 }
