7ff6fea339c096eef555bf7472fd6321254ee898
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-07 17:32:34 +0900

    feat: ArticleImagesRepository 추가

---
+++ src/main/java/com/be05/sns/repository/ArticleImagesRepository.java
@@ -0,0 +1,7 @@
+package com.be05.sns.repository;
+
+import com.be05.sns.entity.ArticleImages;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleImagesRepository extends JpaRepository<ArticleImages, Long> {
+}
