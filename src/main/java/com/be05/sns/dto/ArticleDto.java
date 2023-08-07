d10cf620644cf2c959afc121bfbf963b915063c1
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-07 17:32:18 +0900

    modified: ArticleDto 수정 - @NoArgsConstructor 추가 - userName -> userId 이름 변경 - setter 형식에서 builder 형식으로 변경

--- src/main/java/com/be05/sns/dto/ArticleDto.java
+++ src/main/java/com/be05/sns/dto/ArticleDto.java
@@ -1,34 +1,34 @@
 package com.be05.sns.dto;
 
-import com.be05.sns.entity.Article;
 import com.be05.sns.entity.Users;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class ArticleDto {
     private Long id;
     private String title;
     private String content;
-    private Boolean draft;
     private LocalDateTime createdAt;
     private LocalDateTime deletedAt;
 
-    private String userName;
+    private Users userId;
 
     // 새로운 article 생성
-    public Article newArticle(Users user) {
-        Article article = new Article();
-        article.setUserId(user);
-        article.setTitle(title);
-        article.setContent(content);
-        article.setCreatedAt(LocalDateTime.now(ZoneId.of("Asia/Seoul")));
-        return article;
+    public ArticleDto newArticle(Users user, String title, String content) {
+        return ArticleDto.builder()
+                .userId(user)
+                .title(title)
+                .content(content)
+                .createdAt(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
+                .build();
     }
 }
