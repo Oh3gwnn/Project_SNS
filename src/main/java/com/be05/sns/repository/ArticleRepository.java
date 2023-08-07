1b00ed7d9297e43d3c9ed08ac499531c5390112c
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-08 07:42:13 +0900

    modified: @Repository 추가

--- src/main/java/com/be05/sns/repository/ArticleRepository.java
+++ src/main/java/com/be05/sns/repository/ArticleRepository.java
@@ -4,7 +4,9 @@ import com.be05.sns.entity.Article;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     Page<Article> findAllByUserId_Username(String username, Pageable pageable);
 }
--- src/main/java/com/be05/sns/repository/UserRepository.java
+++ src/main/java/com/be05/sns/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.be05.sns.repository;
 
 import com.be05.sns.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByUsername(String userName);
     Boolean existsByUsername(String userName);
