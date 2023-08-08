b33b6384b3b86a6eb15408bc3df3f0afd5905830
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-08 11:52:11 +0900

    modified: createAt, deleteAt 추가

--- src/main/java/com/be05/sns/entity/Comment.java
+++ src/main/java/com/be05/sns/entity/Comment.java
@@ -3,6 +3,8 @@ package com.be05.sns.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data @Entity
 @Table(name = "comment")
 public class Comment {
@@ -11,6 +13,10 @@ public class Comment {
     private Long id;
     @Column(nullable = false)
     private String content;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 
     @ManyToOne
     @JoinColumn(name = "article_id")
