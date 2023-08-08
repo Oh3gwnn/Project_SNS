fc3abf7c615d81c72aa78c888a050ee9a0c6e031
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-08 09:27:49 +0900

    modified: findByImageUrlContaining 추가 - imageUrls -> /static/이 붙어있어서 Containing으로 함

--- src/main/java/com/be05/sns/repository/ArticleImagesRepository.java
+++ src/main/java/com/be05/sns/repository/ArticleImagesRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface ArticleImagesRepository extends JpaRepository<ArticleImages, Long> {
     ArticleImages findFirstByArticleId_IdOrderByIdAsc(Long articleId);
     List<ArticleImages> findAllByArticleId_Id(Long articleId);
+    ArticleImages findByImageUrlContaining(String fileName);
 }
