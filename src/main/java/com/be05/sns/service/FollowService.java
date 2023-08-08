90ace1edc3c00f747d0c17a4dff83d797ed9523f
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-08 17:07:44 +0900

    modified: unfollow 추가

--- src/main/java/com/be05/sns/service/FollowService.java
+++ src/main/java/com/be05/sns/service/FollowService.java
@@ -6,8 +6,10 @@ import com.be05.sns.entity.Users;
 import com.be05.sns.repository.FollowRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -16,14 +18,37 @@ public class FollowService {
     private final ModelMapper modelMapper;
     private final FollowRepository followRepository;
 
-    public void requestFollow(String username, Authentication authentication) {
+    // 팔로우/언팔로우
+    public String requestFollow(String username, Authentication authentication) {
         Users authUser = getObj.getUser(authentication.getName());
         Users targetUser = getObj.getUser(username);
 
-        //TODO: 자기자신은 팔로우 못함
+        if (authUser.getPassword().equals(targetUser.getPassword()))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
 
-        FollowDto dto = new FollowDto().toFollow(authUser, targetUser);
-        UserFollows follow = modelMapper.map(dto, UserFollows.class);
-        followRepository.save(follow);
+        UserFollows follows = getObj.getFollow(authUser.getUsername(), targetUser.getUsername());
+
+        if (follows == null) {
+            FollowDto dto = new FollowDto().toFollow(authUser, targetUser);
+            UserFollows follow = modelMapper.map(dto, UserFollows.class);
+            followRepository.save(follow);
+            return "님을 팔로우 하셨습니다.";
+        } else {
+            followRepository.delete(follows);
+            return "님을 언팔로우 하셨습니다.";
+        }
+
+    }
+
+    // 상대방이 건 팔로우를 언팔로우
+    public void requestUnfollow(String username, Authentication authentication) {
+        Users authUser = getObj.getUser(authentication.getName());
+        Users targetUser = getObj.getUser(username);
+
+        if (authUser.getPassword().equals(targetUser.getPassword()))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        UserFollows follows = getObj.getFollow(targetUser.getUsername(), authUser.getUsername());
+        followRepository.delete(follows);
     }
 }
