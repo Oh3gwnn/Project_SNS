b62e43284e71da63841fb6b3651d3a73c0af95b5
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-08 15:12:07 +0900

    feat: FollowRepository 추가

---
+++ src/main/java/com/be05/sns/repository/FollowRepository.java
@@ -0,0 +1,7 @@
+package com.be05.sns.repository;
+
+import com.be05.sns.entity.UserFollows;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FollowRepository extends JpaRepository<UserFollows, Long> {
+}
