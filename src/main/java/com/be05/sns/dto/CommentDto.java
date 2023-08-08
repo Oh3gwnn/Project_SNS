73baefc3306e8a29ca54aad9647a627ae996a432
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-08 11:52:20 +0900

    modified: createAt, deleteAt 추가

--- src/main/java/com/be05/sns/dto/CommentDto.java
+++ src/main/java/com/be05/sns/dto/CommentDto.java
@@ -7,6 +7,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -16,12 +19,15 @@ public class CommentDto {
     private String content;
     private Article articleId;
     private Users userId;
+    private LocalDateTime createdAt;
+    private LocalDateTime deletedAt;
 
     public CommentDto newComment(Users user, Article article) {
         return CommentDto.builder()
                 .content(content)
                 .userId(user)
                 .articleId(article)
+                .createdAt(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
                 .build();
     }
 }
