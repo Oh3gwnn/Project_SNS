69243caa4ac55ed498f8bfd5d8cef25d66e62fe0
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-08 17:07:21 +0900

    modified: 해당하는 유저의 follow 목록 불러오기

--- src/main/java/com/be05/sns/repository/FollowRepository.java
+++ src/main/java/com/be05/sns/repository/FollowRepository.java
@@ -4,4 +4,5 @@ import com.be05.sns.entity.UserFollows;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FollowRepository extends JpaRepository<UserFollows, Long> {
+    UserFollows findByFromUser_UsernameAndToUser_Username(String fromUser, String toUser);
 }
