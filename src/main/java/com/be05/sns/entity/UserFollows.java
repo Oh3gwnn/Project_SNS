f99081db96f39e76fa958c667d99293098ac80c9
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-08 17:39:49 +0900

    feat: FollowId 복합키 추가 및 이름, 메서드 수정

--- src/main/java/com/be05/sns/entity/UserFollows.java
+++ src/main/java/com/be05/sns/entity/UserFollows.java
@@ -1,18 +1,28 @@
 package com.be05.sns.entity;
 
+import com.be05.sns.entity.embeddedId.FollowId;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
 
 @Data @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "user_follows")
 public class UserFollows {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @EmbeddedId
+    private FollowId id;
 
-    @ManyToOne
-    private Users fromUser;
+    @MapsId("follower")
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "follower_id")
+    private Users follower;
 
-    @ManyToOne
-    private Users toUser;
+    @MapsId("following")
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "following_id")
+    private Users following;
 }
