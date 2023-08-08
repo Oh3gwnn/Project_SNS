3e0e646d78003f81dfd1bc0957d0c0cce06752d8
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-08 11:15:58 +0900

    modified: FindUser -> GetObjService 변경 - 해당 (User, Article, Comment)를 찾는 메서드 모음

---
+++ src/main/java/com/be05/sns/service/GetObjService.java
@@ -0,0 +1,39 @@
+package com.be05.sns.service;
+
+import com.be05.sns.entity.Article;
+import com.be05.sns.entity.Comment;
+import com.be05.sns.entity.Users;
+import com.be05.sns.repository.ArticleRepository;
+import com.be05.sns.repository.CommentRepository;
+import com.be05.sns.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@RequiredArgsConstructor
+public class GetObjService {
+    private final UserRepository userRepository;
+    private final ArticleRepository articleRepository;
+    private final CommentRepository commentRepository;
+
+    // 해당 유저 불러오기
+    public Users getUser(String userName) {
+        return userRepository.findByUsername(userName)
+                .orElseThrow(() -> new UsernameNotFoundException(userName));
+    }
+
+    // 해당 게시물 불러오기
+    public Article getArticle(Long articleId) {
+        return articleRepository.findById(articleId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    // 해당 댓글 불러오기
+    public Comment getComment(Long commentId) {
+        return commentRepository.findById(commentId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+}
